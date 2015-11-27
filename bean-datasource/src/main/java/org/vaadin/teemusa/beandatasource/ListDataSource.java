f9555b05c2920f188b7e36e479d94e7717032064
Teemu Suo-Anttila <teemusa@vaadin.com>
2015-11-27 18:13:04 +0200

    Add missing implements definition

--- bean-datasource/src/main/java/org/vaadin/teemusa/beandatasource/ListDataSource.java
+++ bean-datasource/src/main/java/org/vaadin/teemusa/beandatasource/ListDataSource.java
@@ -1,78 +1,20 @@
 package org.vaadin.teemusa.beandatasource;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
 
-import org.vaadin.teemusa.TypedComponent;
-import org.vaadin.teemusa.beandatasource.interfaces.DataSource;
 import org.vaadin.teemusa.beandatasource.interfaces.DataSource.HasPaging;
 
-public class ListDataSource<T> implements DataSource<T>, HasPaging<T> {
-
-	protected List<T> repo;
+public class ListDataSource<T> extends CollectionDataSource<T> implements HasPaging<T> {
 
 	private DataProvider<T> dataProvider;
 
 	public ListDataSource(Collection<T> beans) {
-		if (beans instanceof List) {
-			repo = (List<T>) beans;
-		} else {
-			repo = new ArrayList<T>();
-			repo.addAll(beans);
-		}
-	}
-
-	@Override
-	public long size() {
-		return repo.size();
+		super(beans);
 	}
 
 	@Override
 	public Iterable<T> getPage(int start, int end) {
 		return repo.subList(start, end);
-	}
-
-	@Override
-	public DataProvider<T> extend(TypedComponent<T> component) {
-		dataProvider = DataProvider.extend(component, this);
-		return dataProvider;
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return repo.isEmpty();
-	}
-
-	@Override
-	public boolean contains(T o) {
-		return repo.contains(o);
-	}
-
-	@Override
-	public Iterator<T> iterator() {
-		return repo.iterator();
-	}
-
-	@Override
-	public boolean add(T e) {
-		return repo.add(e);
-	}
-
-	@Override
-	public boolean remove(T o) {
-		return repo.remove(o);
-	}
-
-	@Override
-	public void clear() {
-		repo.clear();
-	}
-
-	@Override
-	public void refresh(T e) {
-		dataProvider.updateRowData(e);
 	}
 
 }
